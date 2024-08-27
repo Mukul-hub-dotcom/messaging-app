760323e80deb185d09f6646d4e1cfb1eac2424b6
Subham Kandari <112814727+Subham0629@users.noreply.github.com>
2024-08-27 12:39:14 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 
 // middleware
-app.use(cors(config.cors));
+app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(
   express.urlencoded({

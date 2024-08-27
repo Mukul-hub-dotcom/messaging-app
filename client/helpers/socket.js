f49379141a771a8871fb9cecd74209ee193a5d40
akshatIQ <akshat@instintiq.ai>
2024-08-27 13:10:01 +0530

    .

--- client/helpers/socket.js
+++ client/helpers/socket.js
@@ -2,5 +2,5 @@ import { io } from 'socket.io-client';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const socket = io(isDev ? 'ws://localhost:8080' : '/');
+const socket = io(isDev ? 'ws://13.201.38.154:8080' : '/');
 export default socket;

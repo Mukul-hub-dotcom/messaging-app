1ae83f9da65d580f03ffccf4a42951fd50067219
akshatIQ <akshat@instintiq.ai>
2024-08-27 13:12:45 +0530

    cors condition

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ const isDev = process.env.NODE_ENV === 'development';
 module.exports = {
   isDev,
   cors: {
-    origin: ['*'],
+    origin: ['http://13.201.38.154'],
   },
   db: {
     uri: process.env.MONGO_URI,
